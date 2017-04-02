4919278985879bcd237477216d691b9516ccde48
Philip Weaver <simplygreatwork@gmail.com>
2017-04-02 02:15:41 +0000

    Moving Sequence.js from godsend to godsend-basics.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ module.exports = {
 	Authorizer : require('./Authorizer'),
 	Credentials : require('./Credentials'),
 	Server : require('./Server'),
+	Sequence : require('./Sequence'),
 	Storage : require('./Storage'),
 	Utility : require('./Utility')
 };
